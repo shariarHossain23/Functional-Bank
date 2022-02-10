6f3244aaabcaac8180d6a2b105731329ce24579c
shariarhossain23e <shariarhossain23@gamil.comm>
2022-02-10 23:05:50 +0600

    update total amount

--- bank/bank.js
+++ bank/bank.js
@@ -14,15 +14,38 @@ function updateTotalField(totalField,currentMoney){
     const totalFieldMoney = parseFloat(totalFieldMoneyText);
     totalFieldAmount.innerText = totalFieldMoney + currentMoney;
    }
+function updateTotalAmount(currentBalance,isAdd){
+    const totalBalance = document.getElementById("total-balance");
+    const totalBalanceText = totalBalance.innerText;
+    const updateBalance = parseFloat(totalBalanceText);
+    if(isAdd == true){
+        totalBalance.innerText = updateBalance + currentBalance
+    }
+    else{
+        totalBalance.innerText = updateBalance - currentBalance
+    }
 
+}
 
 //  deposit btn
  document.getElementById('deposit-btn').addEventListener("click",function(){
-    const depositAmount = inputField("deposit-input")
-    updateTotalField("deposit-total",depositAmount)
+    const depositAmount = inputField("deposit-input");
+   if(depositAmount > 0){
+    updateTotalField("deposit-total",depositAmount);
+    updateTotalAmount(depositAmount,true)
+   }
+   else{
+       alert("pls use positive number")
+   }
 });
 // withdraw btn
  document.getElementById("withdraw-btn").addEventListener("click",function(){
    const withdrawAmount = inputField("withdraw-input")
-   updateTotalField("withdraw-total",withdrawAmount)
+    if(withdrawAmount > 0){
+        updateTotalField("withdraw-total",withdrawAmount)
+        updateTotalAmount(withdrawAmount,false)
+    }
+    else{
+        alert("plz use positive Number")
+    }
  })
